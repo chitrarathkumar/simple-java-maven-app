26eabbfae26b5ab7fcc33784df428651a4f66d3d
Chitrarath Kumar <chitrarath.kumar@gmail.com>
2018-09-28 14:50:29 +0530

    Update and rename Jenkinsfile to Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,4 @@
-node {
+/*node {
     docker.withServer('tcp://10.0.3.134:2375'){
         docker.image('maven:3-alpine').inside('-v $HOME/.m2:/root/.m2') {
             stage('Build') {
@@ -17,7 +17,43 @@ node {
             }
         }
     }
+}*/
+node{
+    docker.withServer('tcp://10.0.3.134:2375')
 }
+
+pipeline {
+        agent {
+             docker {
+                image 'maven:3-alpine'
+                args '-v /root/.m2:/root/.m2'
+             }
+        }
+        stages {
+            stage('Build') {
+                steps{
+                    sh 'mvn -B -DskipTests clean package'
+                }
+            }
+            stage('Test') {
+                steps {
+                    sh 'mvn test'
+                }
+                post {
+                    always {
+                        junit 'target/surefire-reports/*.xml'
+                    }
+                }
+            }
+            stage('Deliver') { 
+                steps {
+                    sh './jenkins/scripts/deliver.sh' 
+                }
+            }
+        }
+}
+
+
 /*pipeline {
     agent none
     stages {
