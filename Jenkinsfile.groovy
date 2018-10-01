01cd32d6c8e53a5fa243c1931f89a5224ae34cef
Chitrarath Kumar <chitrarath.kumar@gmail.com>
2018-10-01 09:33:15 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,39 +18,39 @@
         }
     }
 }*/
-node{
-    docker.withServer('tcp://10.0.3.134:2375')
-}
 
 pipeline {
-        agent {
-             docker {
-                image 'maven:3-alpine'
-                args '-v /root/.m2:/root/.m2'
-             }
+    script {
+        docker.withServer('tcp://10.0.3.134:2375')
+    }
+    agent {
+        docker {
+            image 'maven:3-alpine'
+            args '-v /root/.m2:/root/.m2'
         }
-        stages {
-            stage('Build') {
-                steps{
-                    sh 'mvn -B -DskipTests clean package'
-                }
+    }
+    stages {
+        stage('Build') {
+            steps{
+                sh 'mvn -B -DskipTests clean package'
             }
-            stage('Test') {
-                steps {
-                    sh 'mvn test'
-                }
-                post {
-                    always {
-                        junit 'target/surefire-reports/*.xml'
-                    }
-                }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
             }
-            stage('Deliver') { 
-                steps {
-                    sh './jenkins/scripts/deliver.sh' 
+            post {
+                always {
+                    junit 'target/surefire-reports/*.xml'
                 }
             }
         }
+        stage('Deliver') { 
+            steps {
+                sh './jenkins/scripts/deliver.sh' 
+            }
+        }
+    }
 }
 
 
