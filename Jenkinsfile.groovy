0c67e2a9739d3d890fed9d2147d517c853a2242a
Chitrarath Kumar <chitrarath.kumar@gmail.com>
2018-10-01 09:37:42 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,4 @@
-/*node {
+node {
     docker.withServer('tcp://10.0.3.134:2375'){
         docker.image('maven:3-alpine').inside('-v $HOME/.m2:/root/.m2') {
             stage('Build') {
@@ -8,7 +8,7 @@
                 sh 'mvn test'
                 //post {
                     //always {
-                    //    junit 'target/surefire-reports/*.xml'
+                junit 'target/surefire-reports/*.xml'
                   //  }
                 //}
             }
@@ -17,41 +17,41 @@
             }
         }
     }
-}*/
-
-pipeline {
-    agent {   
-        script {
-            docker.withServer('tcp://10.0.3.134:2375')
-        }
-        docker {
-            image 'maven:3-alpine'
-            args '-v /root/.m2:/root/.m2'
-        }
-    }
-    stages {
-        stage('Build') {
-            steps{
-                sh 'mvn -B -DskipTests clean package'
-            }
-        }
-        stage('Test') {
-            steps {
-                sh 'mvn test'
-            }
-            post {
-                always {
-                    junit 'target/surefire-reports/*.xml'
-                }
-            }
-        }
-        stage('Deliver') { 
-            steps {
-                sh './jenkins/scripts/deliver.sh' 
-            }
-        }
-    }
 }
+
+//pipeline {
+  //  agent {   
+    //    script {
+      //      docker.withServer('tcp://10.0.3.134:2375')
+       //}
+        //docker {
+          //  image 'maven:3-alpine'
+            //args '-v /root/.m2:/root/.m2'
+        //}
+    //}
+    //stages {
+      //  stage('Build') {
+        //    steps{
+          //      sh 'mvn -B -DskipTests clean package'
+           // }
+        //}
+        //stage('Test') {
+          //  steps {
+             //   sh 'mvn test'
+           // }
+            //post {
+              //  always {
+                //    junit 'target/surefire-reports/*.xml'
+                //}
+            //}
+        //}
+        //stage('Deliver') { 
+          //  steps {
+            //    sh './jenkins/scripts/deliver.sh' 
+            //}
+        //}
+    //}
+//}
 
 
 /*pipeline {
