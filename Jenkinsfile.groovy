c73dadb60f1ec98480810a9a7afbf58b38073546
Chitrarath Kumar <chitrarath.kumar@gmail.com>
2018-10-03 09:58:14 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -72,10 +72,9 @@
 
 
 
-node {
+/*node {
     docker.withServer('tcp://10.0.3.134:2375'){
         stage('Build') {
-            /* .. snip .. */
         }
 
         stage('Test') {
@@ -87,13 +86,64 @@ node {
                         sh 'make check'
                     }
                     finally {
-                        junit '**/target/*.xml'
+                        junit '/target*.xml'
                     }
                 }
             },
             dockerB: {
                 node('docker') {
-                    /* .. snip .. */
+                }
+            }
+        }
+    }
+}*/
+
+pipeline {
+    agent any
+    stages {
+        stage('Non-Parallel Stage') {
+            steps {
+                echo 'This stage will be executed first.'
+            }
+        }
+        stage('Parallel Stage') {
+            when {
+                branch 'master'
+            }
+            failFast true
+            parallel {
+                stage('Branch A') {
+                    agent {
+                        label "for-branch-a"
+                    }
+                    steps {
+                        echo "On Branch A"
+                    }
+                }
+                stage('Branch B') {
+                    agent {
+                        label "for-branch-b"
+                    }
+                    steps {
+                        echo "On Branch B"
+                    }
+                }
+                stage('Branch C') {
+                    agent {
+                        label "for-branch-c"
+                    }
+                    stages {
+                        stage('Nested 1') {
+                            steps {
+                                echo "In stage Nested 1 within Branch C"
+                            }
+                        }
+                        stage('Nested 2') {
+                            steps {
+                                echo "In stage Nested 2 within Branch C"
+                            }
+                        }
+                    }
                 }
             }
         }
