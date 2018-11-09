903501132287ded5a23bccfb5f7e5283b3d4c751
Chitrarath Kumar <chitrarath.kumar@gmail.com>
2018-11-09 14:50:57 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -32,7 +32,7 @@ pipeline {
                 stage('Build') {
                     steps {
                         sh 'mvn -B -DskipTests clean package'
-                        sh '/usr/local/bin/docker-compose --help'
+                        //sh '/usr/local/bin/docker-compose --help'
                         sh 'docker run hello-world'
                     }
                 }
